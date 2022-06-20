a07d04d5ba2c387823f9aca8c33742901508f9b9
Katia Guzmán Martínez <katia.guzmart@gmail.com>
2022-06-20 09:36:00 -0500

    [REPO] Logos en tabla complete

--- 03_códigos/01_03_01_monitoreo.R
+++ 03_códigos/01_03_01_monitoreo.R
@@ -97,12 +97,12 @@ mcv_discrete_12 <- c("#4D5BF0", "#0ACF5F", "#E84D9A", "#E8866D",
 # Token para API del INEGI
 source(paste_code("00_token.R"))
 
-# 1. Procesamiento para tabla --------------------------------------------------
+# 1. Procesamiento para tabla de variación --------------------------------------------------
 ## 1.0. Abrir INPC complete ----
 d_inpc <- readRDS(paste_out("01_03_inpc_complete_prods_ccif.RDS")) %>% 
     glimpse()
 
-v_quincena <- 1
+v_quincena <- 2
 ## 1.1. Identificadores de productos para seguimiento ----
 v_prods_suby <- c(
     "01_011_0111_014", 
@@ -227,16 +227,14 @@ v_productos <- sort(unique(d_monitoreo$ccif))
 
 # Clasificación según tipo de inflación 
 v_subyacente <- c(
-    v_productos[19], v_productos[12], v_productos[1 ], v_productos[15], 
-    v_productos[14], v_productos[11], v_productos[20], v_productos[17], 
-    v_productos[18], v_productos[3 ], v_productos[21])
+    v_productos[20], v_productos[13], v_productos[1], v_productos[16], 
+    v_productos[15], v_productos[12], v_productos[21], v_productos[18], 
+    v_productos[19], v_productos[3], v_productos[22])
 
 v_nosubyacente <- v_productos[!(v_productos %in% v_subyacente)]
 
 # Procesamiento (formato largo)
 df_formato <- d_monitoreo %>% 
-    # Dejar solo datos de la última actualización 
-    filter(fecha == max(fecha)) %>% 
     # Crear variables para tabla (Markdown y html)
     mutate(
         # Símbolo de cambio 
@@ -261,14 +259,31 @@ df_formato <- d_monitoreo %>%
         ), 
         # Logotipo para la tabla
         logo = case_when(
-            ccif == v_productos[1 ] ~ pegar_logo("Aceite"),
-            ccif == v_productos[12] ~ pegar_logo("Jugos"),
-            ccif == v_productos[13] ~ pegar_logo("Leche"),
-            ccif == v_productos[16] ~ pegar_logo("PanDeCaja"),
-            ccif == v_productos[18] ~ pegar_logo("Restaurantes"),
-            ccif == v_productos[15] ~ pegar_logo("Taqueria"),
-            ccif == v_productos[20] ~ pegar_logo("Tortilla"), 
-            ccif == v_productos[21] ~ pegar_logo("Transporte_aereo"))) %>% 
+            ccif == v_productos[1] ~ pegar_logo("01_Aceite"),
+            ccif == v_productos[2] ~ pegar_logo("02_Aguacate"),
+            ccif == v_productos[3] ~ pegar_logo("03_Automovil"),
+            ccif == v_productos[4] ~ pegar_logo("04_Res"),
+            ccif == v_productos[5] ~ pegar_logo("05_Cebolla"),
+            ccif == v_productos[6] ~ pegar_logo("06_Serrano"),
+            ccif == v_productos[7] ~ pegar_logo("07_Electricidad"),
+            ccif == v_productos[8] ~ pegar_logo("08_Gas"),
+            ccif == v_productos[9] ~ pegar_logo("09_Gasolina"),
+            ccif == v_productos[10] ~ pegar_logo("10_Huevo"),
+            ccif == v_productos[11] ~ pegar_logo("11_Jitomate"),
+            ccif == v_productos[12] ~ pegar_logo("12_Jugos"),
+            ccif == v_productos[13] ~ pegar_logo("13_Leche"),
+            ccif == v_productos[14] ~ pegar_logo("14_Limon"),
+            ccif == v_productos[15] ~ pegar_logo("15_Taqueria"),
+            ccif == v_productos[16] ~ pegar_logo("16_PanDeCaja"),
+            ccif == v_productos[17] ~ pegar_logo("17_Pollo"),
+            ccif == v_productos[18] ~ pegar_logo("18_Restaurantes"),
+            ccif == v_productos[19] ~ pegar_logo("19_Paquete_turistico"),
+            ccif == v_productos[20] ~ pegar_logo("20_Tortilla"),
+            ccif == v_productos[21] ~ pegar_logo("21_Transporte_aereo"),
+            ccif == v_productos[22] ~ pegar_logo("22_Vivienda")
+        )) %>% 
+    # Dejar solo datos de la última actualización 
+    filter(fecha == max(fecha)) %>% 
     # Distinguir entre productos subyacentes y no subyacentes
     mutate(tipo = if_else(
         ccif %in% v_subyacente, "subyacente", "nosubyacente")) %>% 
@@ -285,7 +300,7 @@ df_web <- df_formato                %>%
         by = "id")            %>% 
     select(-c(starts_with("id"), starts_with("tipo")))
 
-# 2. Guardar -------------------------------------------------------------------
+## 1.4. Guardar en Drive -------------------------------------------------------------------
 
 # Obtener identificador del archivo en drive
 v_id <- as.character(
@@ -295,8 +310,94 @@ v_id <- as.character(
 # Escribir datos en drive 
 googlesheets4::write_sheet(ss = v_id, data = df_web, sheet = "tasas")
 
-# 3. Republicar en DW
+## 1.5. Republicar en DW ----
 
 DatawRappr::dw_publish_chart(chart_id = "zt54l", api_key = dw_token)
+
+# 2. Procesamiento para tabla de incidencia --------------------------------------------------
+## 2.3. Preparación web -----
+
+
+# Procesamiento (formato largo)
+df_formato <- d_monitoreo %>% 
+    # Dejar solo datos de la última actualización 
+    filter(fecha == max(fecha)) %>% 
+    # Crear variables para tabla (Markdown y html)
+    mutate(
+        # Símbolo de cambio 
+        cambio = case_when(
+            incidencia_anual >  0 ~ "▲",
+            incidencia_anual <  0 ~ "▼",
+            incidencia_anual == 0 ~ "~" ),
+        # HTML
+        # var_anual >  0 ~ "<font color = \"#00b783\"> ▲ </font>",
+        # var_anual <  0 ~ "<font color = \"#ff6260\"> ▼ </font>",
+        # var_anual == 0 ~ "<font color = \"#777777\"> ~ </font>"), 
+        # CSS
+        # var_anual >  0 ~ "<p style = \"color:#00b783\"> ▲ </p>",
+        # var_anual <  0 ~ "<p style = \"color:#ff6260\"> ▼ </p>",
+        # var_anual == 0 ~ "<p style = \"color:#777777\"> ~ </p>"),
+        # Texto para la tabla
+        texto = paste0(
+            # Nombre del producto en negritas 
+            "**", ccif,"**", "<br>", 
+            "Anual: "  , scales::number(incidencia_anual  , accuracy = 0.001), "<br>", 
+            "Mensual: ", scales::number(incidencia_mensual, accuracy = 0.001)
+        ), 
+        # Logotipo para la tabla
+        logo = case_when(
+            ccif == v_productos[1] ~ pegar_logo("01_Aceite"),
+            ccif == v_productos[2] ~ pegar_logo("02_Aguacate"),
+            ccif == v_productos[3] ~ pegar_logo("03_Automovil"),
+            ccif == v_productos[4] ~ pegar_logo("04_Res"),
+            ccif == v_productos[5] ~ pegar_logo("05_Cebolla"),
+            ccif == v_productos[6] ~ pegar_logo("06_Serrano"),
+            ccif == v_productos[7] ~ pegar_logo("07_Electricidad"),
+            ccif == v_productos[8] ~ pegar_logo("08_Gas"),
+            ccif == v_productos[9] ~ pegar_logo("09_Gasolina"),
+            ccif == v_productos[10] ~ pegar_logo("10_Huevo"),
+            ccif == v_productos[11] ~ pegar_logo("11_Jitomate"),
+            ccif == v_productos[12] ~ pegar_logo("12_Jugos"),
+            ccif == v_productos[13] ~ pegar_logo("13_Leche"),
+            ccif == v_productos[14] ~ pegar_logo("14_Limon"),
+            ccif == v_productos[15] ~ pegar_logo("15_Taqueria"),
+            ccif == v_productos[16] ~ pegar_logo("16_PanDeCaja"),
+            ccif == v_productos[17] ~ pegar_logo("17_Pollo"),
+            ccif == v_productos[18] ~ pegar_logo("18_Restaurantes"),
+            ccif == v_productos[19] ~ pegar_logo("19_Paquete_turistico"),
+            ccif == v_productos[20] ~ pegar_logo("20_Tortilla"),
+            ccif == v_productos[21] ~ pegar_logo("21_Transporte_aereo"),
+            ccif == v_productos[22] ~ pegar_logo("22_Vivienda")
+        )) %>% 
+    # Distinguir entre productos subyacentes y no subyacentes
+    mutate(tipo = if_else(
+        ccif %in% v_subyacente, "subyacente", "nosubyacente")) %>% 
+    select(tipo, logo, texto, cambio) 
+
+# Cambiar a formato ancho (distinguir subyacente y no subyacente)
+df_web <- df_formato                %>% 
+    filter(tipo == "subyacente")    %>% 
+    mutate(id = 1:11)               %>%  # El id solo sirve para pegar
+    left_join(
+        df_formato            %>% 
+            filter(tipo != "subyacente") %>% 
+            mutate(id = 1:11), 
+        by = "id")            %>% 
+    select(-c(starts_with("id"), starts_with("tipo")))
+
+## 2.4. Guardar en Drive -------------------------------------------------------------------
+
+# Obtener identificador del archivo en drive
+v_id <- as.character(
+    googledrive::drive_get(
+        "https://docs.google.com/spreadsheets/d/1r1etquU3ClNcyOf8gxJDn3gqGGWYZH1T5EGxe5GdOB8/edit#gid=0")[1, 2])
+
+# Escribir datos en drive 
+googlesheets4::write_sheet(ss = v_id, data = df_web, sheet = "incidencia")
+
+## 2.5. Republicar en DW ----
+
+DatawRappr::dw_publish_chart(chart_id = "61FSx", api_key = dw_token)
+
 
 # FIN --------------------------------------------------------------------------
