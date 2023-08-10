8607d40832d6872790091af1b7e698b6cde34f9e
Axel-orio <66447359+Axel-orio@users.noreply.github.com>
2023-08-09 18:35:18 -0600

    Update 01_03_02_monitoreo.R

--- 03_códigos/01_03_02_monitoreo.R
+++ 03_códigos/01_03_02_monitoreo.R
@@ -109,7 +109,7 @@ source(paste_code("00_token.R"))
 d_inpc <- readRDS(paste_out("01_03_inpc_complete_prods_ccif.RDS")) %>% 
     glimpse()
 
-v_quincena <- 1
+v_quincena <- 2
 
 ## 1.1. Identificadores de productos para seguimiento ----
 v_prods_suby <- c(
