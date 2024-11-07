7c94336de4dcbcdad0a659b82e7b7cd32e609ce4
Axel-orio <66447359+Axel-orio@users.noreply.github.com>
2024-11-07 15:17:51 -0600

    Update 01_03_02_monitoreo.R

--- 03_códigos/01_03_02_monitoreo.R
+++ 03_códigos/01_03_02_monitoreo.R
@@ -50,12 +50,12 @@ require(tidyverse)
 ## Credenciales de google ----
 # v_usuaria <- "regina"
 # v_usuaria <- "katia"
-v_usuaria <- "juvenal"
-# v_usuaria <- "axel"
+# v_usuaria <- "juvenal"
+v_usuaria <- "axel"
 
 # SELECCIONAR QUINCENA !!!!!!!!!!!!
 ####################################
-v_quincena <- 1
+v_quincena <- 2
 ####################################
 
 googledrive::drive_auth(paste0(v_usuaria, "@mexicocomovamos.mx"))
