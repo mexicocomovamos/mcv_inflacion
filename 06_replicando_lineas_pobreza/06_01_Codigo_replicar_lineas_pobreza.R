a2e7e893bc4cb06220ef264ccc990dba9c6aaebe
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2025-06-06 12:02:05 -0600

    [REPO] Ajustes código lineas de pobreza

--- 06_replicando_lineas_pobreza/06_01_Codigo_replicar_lineas_pobreza.R
+++ 06_replicando_lineas_pobreza/06_01_Codigo_replicar_lineas_pobreza.R
@@ -110,219 +110,219 @@ fecha_sel <- "2025-03-01"
 
 gen_lineas_pobreza <- function(fecha_sel){
 
-# LPEI_rural ----
-c_alim_rur <- c_alim %>% 
-  filter(!is.na(consumo_r)) %>% 
-  arrange(clave) 
-
-inpc_ago_16 <-   inpc2 %>% 
-  mutate(is.ago_2016 = ifelse(date == "2016-08-01", yes = T, no = F)) %>% 
-  filter(is.ago_2016) %>% 
-  select(clave, clave_inpc, inpc_ago_2016 = values)
-  
-deflactores <- inpc2 %>% 
-  left_join(inpc_ago_16, relationship = "many-to-many") %>% 
-  ungroup() %>% 
-  group_by(date, clave) %>% 
-  summarise(values = mean(values), 
-            inpc_ago_2016 = mean(inpc_ago_2016)) %>% 
-  mutate(def = values/inpc_ago_2016) 
-
-lpei_r <- c_alim_rur %>% 
-  select(-contains("_18")) %>% 
-  left_join(deflactores, by = c("clave")) %>% 
-  filter(date == fecha_sel) %>% 
-  select(nombres, consumo_r, clave, precio_mg_r, def) %>% 
-  group_by(nombres, clave) %>% 
-  summarise(consumo_r = mean(consumo_r, na.rm = T), 
-            precio_mg_r = mean(precio_mg_r, na.rm = T), 
-            def = mean(def)) %>% 
-  mutate(precio = precio_mg_r*def) %>% 
-  arrange(clave) 
-
-## Casos especiales ----
-
-### Chiles rural ----
-chiles_rural <- lpei_r %>% 
-    filter(clave %in% c("A115", "A116", "A117", "A118")) %>% 
-    mutate(nombres =  "Chile", clave = "AChile") %>% 
-    group_by(clave, nombres) %>%
-    summarise(consumo_r = sum(consumo_r),
-              precio = mean(precio)) %>%
-    mutate(valor_monetario_diario = (consumo_r/1000)*precio) %>%
-    mutate(valor_monetario_mensual = valor_monetario_diario*30)
-
-### Comida afuera ----
-comida_fuera_rural <- lpei_r %>% 
-  filter(clave %in% c("A243", "A244", "A245")) %>% 
-  mutate(valor_monetario_diario = (consumo_r/1000)*precio) %>%
-  ungroup() %>% 
-  summarise(nombres = "Alimentos y bebidas consumidos fuera del hogar", 
-            clave = "A243-A245", 
-            consumo_r = sum(consumo_r),
-            precio_mg_r = sum(precio_mg_r), 
-            def = mean(def),
-            valor_monetario_diario = sum(valor_monetario_diario), 
-            valor_monetario_mensual = valor_monetario_diario*30) %>% 
-  select(-precio_mg_r, -def)
-
-### Todo lo demás ----
-todos_demas_rural <- lpei_r %>% 
-  filter(!(clave %in% c("A243", "A244", "A245", "A115", "A116", "A117", "A118"))) %>% 
-  group_by(clave, nombres) %>%
-  summarise(consumo_r = sum(consumo_r),
-            precio = mean(precio)) %>%
-  mutate(valor_monetario_diario = (consumo_r/1000)*precio) %>%
-  mutate(valor_monetario_mensual = valor_monetario_diario*30)
-
-### Juntamos ----
-lpei_r2 <- rbind(todos_demas_rural, comida_fuera_rural, chiles_rural)
-lpei_r2_numero = sum(lpei_r2$valor_monetario_mensual) %>% round(2)
-linea_pobreza_extrema_rural = lpei_r2_numero
-
-
-# LPEI URBANO ----
-c_alim_urb <- c_alim %>% 
-  filter(!is.na(consumo_u)) %>% 
-  arrange(clave) 
-
-inpc_ago_16 <-   inpc2 %>% 
-  left_join(eq_enigh %>% select(clave, cve_inpc_24), by = c("clave" = "clave", 
-                                                            "clave_inpc" = "cve_inpc_24")) %>%
-  mutate(is.ago_2016 = ifelse(date == "2016-08-01", yes = T, no = F)) %>% 
-  filter(is.ago_2016) %>% 
-  select(clave, clave_inpc, inpc_ago_2016 = values)
-
-lpei_u <- c_alim_urb %>% 
-  select(-contains("_18")) %>% 
-  left_join(deflactores, by = c("clave")) %>% 
-  filter(date == fecha_sel) %>% 
-  select(nombres, consumo_u, clave, precio_mg_u, def) %>% 
-  group_by(nombres, clave) %>% 
-  summarise(consumo_u = mean(consumo_u, na.rm = T), 
-            precio_mg_u = mean(precio_mg_u, na.rm = T), 
-            def = mean(def)) %>% 
-  mutate(precio = precio_mg_u*def) %>% 
-  arrange(clave) 
-
-## Casos especiales ----
-
-### Chiles ----
-chiles_urb <- lpei_u %>% 
-  filter(clave %in% c("A115", "A116", "A117", "A118")) %>% 
-  mutate(nombres =  "Chile", clave = "AChile") %>% 
-  group_by(clave, nombres) %>%
-  summarise(consumo_u = sum(consumo_u),
-            precio = mean(precio)) %>%
-  mutate(valor_monetario_diario = (consumo_u/1000)*precio) %>%
-  mutate(valor_monetario_mensual = valor_monetario_diario*30)
-
-### Comida fuera ----
-comida_fuera_urb <- lpei_u %>% 
-  filter(clave %in% c("A243", "A244", "A245")) %>% 
-  mutate(valor_monetario_diario = (consumo_u/1000)*precio) %>%
-  ungroup() %>% 
-  summarise(nombres = "Alimentos y bebidas consumidos fuera del hogar", 
-            clave = "A243-A245", 
-            consumo_u = sum(consumo_u),
-            precio_mg_u = sum(precio_mg_u), 
-            def = mean(def),
-            valor_monetario_diario = sum(valor_monetario_diario), 
-            valor_monetario_mensual = valor_monetario_diario*30) %>% 
-  select(-precio_mg_u, -def)
-
-### Todo lo demás ----
-todos_demas_urb <- lpei_u %>% 
-  filter(!(clave %in% c("A243", "A244", "A245", "A115", "A116", "A117", "A118"))) %>% 
-  group_by(clave, nombres) %>%
-  summarise(consumo_u = sum(consumo_u),
-            precio = mean(precio)) %>%
-  mutate(valor_monetario_diario = (consumo_u/1000)*precio) %>%
-  mutate(valor_monetario_mensual = valor_monetario_diario*30)
-
-lpei_u2 <- rbind(todos_demas_urb, comida_fuera_urb, chiles_urb)
-lpei_u2_numero = sum(round(lpei_u2$valor_monetario_mensual, 2))
-linea_pobreza_extrema_urbana = lpei_u2_numero
-
-
-# LÍNEA DE POBREZA POR INGRESOS ----
-# Canasta no alimentaria --- 
-
-# Urbana ----
-deflactores <- inpc2 %>% 
-  left_join(inpc_ago_16, relationship = "many-to-many") %>% 
-  ungroup() %>% 
-  group_by(date, clave) %>% 
-  summarise(values = mean(values), 
-            inpc_ago_2016 = mean(inpc_ago_2016)) %>% 
-  mutate(def = values/inpc_ago_2016) 
-
-td <- c_n_alim %>% 
-  left_join(deflactores) %>% 
-  filter(date == fecha_sel) 
-
-td2_urbana <- td %>% 
-  mutate(costo = gib_16_urbano*def) %>%
-  group_by(grupo2) %>%
-  summarise(costo = sum(costo, na.rm = T)) %>% 
-  mutate(grupo2 = factor(grupo2, levels = c(
-    "Transporte público",
-    "Limpieza y cuidados de la casa",                      
-    "Cuidados personales",                                
-    "Educación, cultura y recreación",                    
-    "Comunicaciones y servicios para vehículos",
-    "Vivienda y servicios de conservación",
-    "Prendas de vestir, calzado y accesorios",
-    "Cristalería, blancos y utensilios domésticos",
-    "Cuidados de la salud",
-    "Enseres domésticos y mantenimiento de la vivienda",
-    "Artículos de esparcimiento",
-    "Transporte",
-    "Otros gastos"))) %>% 
-  arrange(grupo2)
-
-linea_pobreza_urbana = sum(td2_urbana$costo) %>% round(2) # Debe de dar $2,210.32
-
-
-td2_rural <- td %>% 
-  mutate(costo = gib_16_rural*def) %>%
-  group_by(grupo2) %>%
-  summarise(costo = sum(costo, na.rm = T)) %>% 
-  mutate(grupo2 = factor(grupo2, levels = c(
-    "Transporte público",
-    "Limpieza y cuidados de la casa",                      
-    "Cuidados personales",                                
-    "Educación, cultura y recreación",                    
-    "Comunicaciones y servicios para vehículos",
-    "Vivienda y servicios de conservación",               
-    "Prendas de vestir, calzado y accesorios",
-    "Cristalería, blancos y utensilios domésticos",       
-    "Cuidados de la salud",                               
-    "Enseres domésticos y mantenimiento de la vivienda",  
-    "Artículos de esparcimiento",
-    "Transporte",                                         
-    "Otros gastos"))) %>% 
-  arrange(grupo2)
-
-linea_pobreza_rural = sum(td2_rural$costo) %>% round(2) # Debe de dar $2,210.32
-
-# Armamos la tabla
-Anio = fecha_sel %>% year()
-Mes = fecha_sel %>% month() %>% str_pad(width = 2, side = "left", pad = "0")
-Mes_texto = fecha_sel %>% month(label = T) %>% str_to_sentence()
-linea_pobreza_extrema_urbana
-linea_pobreza_extrema_rural
-linea_pobreza_urbana2 = linea_pobreza_extrema_urbana + linea_pobreza_urbana
-linea_pobreza_rural2 = linea_pobreza_rural + linea_pobreza_extrema_rural
-
-tabla_resultado <- tibble(`Año` = Anio, 
-       Mes = Mes, 
-       `LPEI rural` = linea_pobreza_extrema_rural, 
-       `LPEI urbano` = linea_pobreza_extrema_urbana, 
-       `LPI rural` = linea_pobreza_rural2, 
-       `LPI urbano` = linea_pobreza_urbana2)
-
-return(tabla_resultado)
+        # LPEI_rural ----
+        c_alim_rur <- c_alim %>% 
+          filter(!is.na(consumo_r)) %>% 
+          arrange(clave) 
+        
+        inpc_ago_16 <-   inpc2 %>% 
+          mutate(is.ago_2016 = ifelse(date == "2016-08-01", yes = T, no = F)) %>% 
+          filter(is.ago_2016) %>% 
+          select(clave, clave_inpc, inpc_ago_2016 = values)
+          
+        deflactores <- inpc2 %>% 
+          left_join(inpc_ago_16, relationship = "many-to-many") %>% 
+          ungroup() %>% 
+          group_by(date, clave) %>% 
+          summarise(values = mean(values), 
+                    inpc_ago_2016 = mean(inpc_ago_2016)) %>% 
+          mutate(def = values/inpc_ago_2016) 
+        
+        lpei_r <- c_alim_rur %>% 
+          select(-contains("_18")) %>% 
+          left_join(deflactores, by = c("clave")) %>% 
+          filter(date == fecha_sel) %>% 
+          select(nombres, consumo_r, clave, precio_mg_r, def) %>% 
+          group_by(nombres, clave) %>% 
+          summarise(consumo_r = mean(consumo_r, na.rm = T), 
+                    precio_mg_r = mean(precio_mg_r, na.rm = T), 
+                    def = mean(def)) %>% 
+          mutate(precio = precio_mg_r*def) %>% 
+          arrange(clave) 
+        
+        ## Casos especiales ----
+        
+        ### Chiles rural ----
+        chiles_rural <- lpei_r %>% 
+            filter(clave %in% c("A115", "A116", "A117", "A118")) %>% 
+            mutate(nombres =  "Chile", clave = "AChile") %>% 
+            group_by(clave, nombres) %>%
+            summarise(consumo_r = sum(consumo_r),
+                      precio = mean(precio)) %>%
+            mutate(valor_monetario_diario = (consumo_r/1000)*precio) %>%
+            mutate(valor_monetario_mensual = valor_monetario_diario*30)
+        
+        ### Comida afuera ----
+        comida_fuera_rural <- lpei_r %>% 
+          filter(clave %in% c("A243", "A244", "A245")) %>% 
+          mutate(valor_monetario_diario = (consumo_r/1000)*precio) %>%
+          ungroup() %>% 
+          summarise(nombres = "Alimentos y bebidas consumidos fuera del hogar", 
+                    clave = "A243-A245", 
+                    consumo_r = sum(consumo_r),
+                    precio_mg_r = sum(precio_mg_r), 
+                    def = mean(def),
+                    valor_monetario_diario = sum(valor_monetario_diario), 
+                    valor_monetario_mensual = valor_monetario_diario*30) %>% 
+          select(-precio_mg_r, -def)
+        
+        ### Todo lo demás ----
+        todos_demas_rural <- lpei_r %>% 
+          filter(!(clave %in% c("A243", "A244", "A245", "A115", "A116", "A117", "A118"))) %>% 
+          group_by(clave, nombres) %>%
+          summarise(consumo_r = sum(consumo_r),
+                    precio = mean(precio)) %>%
+          mutate(valor_monetario_diario = (consumo_r/1000)*precio) %>%
+          mutate(valor_monetario_mensual = valor_monetario_diario*30)
+        
+        ### Juntamos ----
+        lpei_r2 <- rbind(todos_demas_rural, comida_fuera_rural, chiles_rural)
+        lpei_r2_numero = sum(lpei_r2$valor_monetario_mensual) %>% round(2)
+        linea_pobreza_extrema_rural = lpei_r2_numero
+        
+        
+        # LPEI URBANO ----
+        c_alim_urb <- c_alim %>% 
+          filter(!is.na(consumo_u)) %>% 
+          arrange(clave) 
+        
+        inpc_ago_16 <-   inpc2 %>% 
+          left_join(eq_enigh %>% select(clave, cve_inpc_24), by = c("clave" = "clave", 
+                                                                    "clave_inpc" = "cve_inpc_24")) %>%
+          mutate(is.ago_2016 = ifelse(date == "2016-08-01", yes = T, no = F)) %>% 
+          filter(is.ago_2016) %>% 
+          select(clave, clave_inpc, inpc_ago_2016 = values)
+        
+        lpei_u <- c_alim_urb %>% 
+          select(-contains("_18")) %>% 
+          left_join(deflactores, by = c("clave")) %>% 
+          filter(date == fecha_sel) %>% 
+          select(nombres, consumo_u, clave, precio_mg_u, def) %>% 
+          group_by(nombres, clave) %>% 
+          summarise(consumo_u = mean(consumo_u, na.rm = T), 
+                    precio_mg_u = mean(precio_mg_u, na.rm = T), 
+                    def = mean(def)) %>% 
+          mutate(precio = precio_mg_u*def) %>% 
+          arrange(clave) 
+        
+        ## Casos especiales ----
+        
+        ### Chiles ----
+        chiles_urb <- lpei_u %>% 
+          filter(clave %in% c("A115", "A116", "A117", "A118")) %>% 
+          mutate(nombres =  "Chile", clave = "AChile") %>% 
+          group_by(clave, nombres) %>%
+          summarise(consumo_u = sum(consumo_u),
+                    precio = mean(precio)) %>%
+          mutate(valor_monetario_diario = (consumo_u/1000)*precio) %>%
+          mutate(valor_monetario_mensual = valor_monetario_diario*30)
+        
+        ### Comida fuera ----
+        comida_fuera_urb <- lpei_u %>% 
+          filter(clave %in% c("A243", "A244", "A245")) %>% 
+          mutate(valor_monetario_diario = (consumo_u/1000)*precio) %>%
+          ungroup() %>% 
+          summarise(nombres = "Alimentos y bebidas consumidos fuera del hogar", 
+                    clave = "A243-A245", 
+                    consumo_u = sum(consumo_u),
+                    precio_mg_u = sum(precio_mg_u), 
+                    def = mean(def),
+                    valor_monetario_diario = sum(valor_monetario_diario), 
+                    valor_monetario_mensual = valor_monetario_diario*30) %>% 
+          select(-precio_mg_u, -def)
+        
+        ### Todo lo demás ----
+        todos_demas_urb <- lpei_u %>% 
+          filter(!(clave %in% c("A243", "A244", "A245", "A115", "A116", "A117", "A118"))) %>% 
+          group_by(clave, nombres) %>%
+          summarise(consumo_u = sum(consumo_u),
+                    precio = mean(precio)) %>%
+          mutate(valor_monetario_diario = (consumo_u/1000)*precio) %>%
+          mutate(valor_monetario_mensual = valor_monetario_diario*30)
+        
+        lpei_u2 <- rbind(todos_demas_urb, comida_fuera_urb, chiles_urb)
+        lpei_u2_numero = sum(round(lpei_u2$valor_monetario_mensual, 2))
+        linea_pobreza_extrema_urbana = lpei_u2_numero
+        
+        
+        # LÍNEA DE POBREZA POR INGRESOS ----
+        # Canasta no alimentaria --- 
+        
+        # Urbana ----
+        deflactores <- inpc2 %>% 
+          left_join(inpc_ago_16, relationship = "many-to-many") %>% 
+          ungroup() %>% 
+          group_by(date, clave) %>% 
+          summarise(values = mean(values), 
+                    inpc_ago_2016 = mean(inpc_ago_2016)) %>% 
+          mutate(def = values/inpc_ago_2016) 
+        
+        td <- c_n_alim %>% 
+          left_join(deflactores) %>% 
+          filter(date == fecha_sel) 
+        
+        td2_urbana <- td %>% 
+          mutate(costo = gib_16_urbano*def) %>%
+          group_by(grupo2) %>%
+          summarise(costo = sum(costo, na.rm = T)) %>% 
+          mutate(grupo2 = factor(grupo2, levels = c(
+            "Transporte público",
+            "Limpieza y cuidados de la casa",                      
+            "Cuidados personales",                                
+            "Educación, cultura y recreación",                    
+            "Comunicaciones y servicios para vehículos",
+            "Vivienda y servicios de conservación",
+            "Prendas de vestir, calzado y accesorios",
+            "Cristalería, blancos y utensilios domésticos",
+            "Cuidados de la salud",
+            "Enseres domésticos y mantenimiento de la vivienda",
+            "Artículos de esparcimiento",
+            "Transporte",
+            "Otros gastos"))) %>% 
+          arrange(grupo2)
+        
+        linea_pobreza_urbana = sum(td2_urbana$costo) %>% round(2) # Debe de dar $2,210.32
+        
+        
+        td2_rural <- td %>% 
+          mutate(costo = gib_16_rural*def) %>%
+          group_by(grupo2) %>%
+          summarise(costo = sum(costo, na.rm = T)) %>% 
+          mutate(grupo2 = factor(grupo2, levels = c(
+            "Transporte público",
+            "Limpieza y cuidados de la casa",                      
+            "Cuidados personales",                                
+            "Educación, cultura y recreación",                    
+            "Comunicaciones y servicios para vehículos",
+            "Vivienda y servicios de conservación",               
+            "Prendas de vestir, calzado y accesorios",
+            "Cristalería, blancos y utensilios domésticos",       
+            "Cuidados de la salud",                               
+            "Enseres domésticos y mantenimiento de la vivienda",  
+            "Artículos de esparcimiento",
+            "Transporte",                                         
+            "Otros gastos"))) %>% 
+          arrange(grupo2)
+        
+        linea_pobreza_rural = sum(td2_rural$costo) %>% round(2) # Debe de dar $2,210.32
+        
+        # Armamos la tabla
+        Anio = fecha_sel %>% year()
+        Mes = fecha_sel %>% month() %>% str_pad(width = 2, side = "left", pad = "0")
+        Mes_texto = fecha_sel %>% month(label = T) %>% str_to_sentence()
+        linea_pobreza_extrema_urbana
+        linea_pobreza_extrema_rural
+        linea_pobreza_urbana2 = linea_pobreza_extrema_urbana + linea_pobreza_urbana
+        linea_pobreza_rural2 = linea_pobreza_rural + linea_pobreza_extrema_rural
+        
+        tabla_resultado <- tibble(`Año` = Anio, 
+               Mes = Mes, 
+               `LPEI rural` = linea_pobreza_extrema_rural, 
+               `LPEI urbano` = linea_pobreza_extrema_urbana, 
+               `LPI rural` = linea_pobreza_rural2, 
+               `LPI urbano` = linea_pobreza_urbana2)
+        
+        return(tabla_resultado)
 
 }
 
@@ -340,7 +340,7 @@ fechas_primeros <- seq(from = primer_dia_actual,
 # Mostrar el vector resultante
 fechas_primeros
 
-f = fechas_primeros[1]
+# f = fechas_primeros[1]
 datos_lineas_pobreza <- lapply(fechas_primeros, function(f){
   tryCatch({
     lineas <- gen_lineas_pobreza(fecha_sel = f)
@@ -357,7 +357,8 @@ datos_lineas_final <- datos_lineas_pobreza %>%
   do.call(rbind, .) %>% 
   arrange(Año, as.numeric(Mes))
 
+# Guardamos el dato 
 write_csv(datos_lineas_final, "01_09_lpei_coneval.csv")
-# write_csv(datos_lineas_final, "01_09_lpei_coneval.csv")
-
+# Guardamos el dato en el repo de Semáforos
+write_csv(datos_lineas_final, "../../mcv_semaforos/01_datos_crudos/01_09_lpei_coneval.csv")
 
